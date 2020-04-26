0b22d3c135610707965246031a9029e317a2db90
Kasia Romaniuk <k.romaniuk99@gmail.com>
2020-04-26 16:44:37 +0200

    Fixing the inital graph, adding second graph

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(input_values, squares, linewidth=5)
+
+# Define the title and labels
+plt.title("Numbers squares", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square value", fontsize=14)
+
+# Define the size of the labels
+plt.tick_params(axis='both', labelsize=14)
+
 plt.show()
